a2beb9bf2e205b1c87b95defd8a35315b656c103
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-20 23:59:49 +0900

    :sparkles: 3-1 ListAddコンポーネントを作成しよう 1.「フォームボタンが押下されたら、リストを作成する」仕組みを実装しよう（ListAdd.vue）

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,12 +5,23 @@
     </header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <!-- 
+        下記のようなエラーが出た……
+          17:5  error  The "ListAdd" component has been registered but not used  vue/no-unused-components 
+        <list-ladd />と記述していなかったためエラーがでたようだ
+        componentsとして読み込んでも使用していないとエラーになるようだ
+      -->
+      <list-add />
     </main>
   </div>
 </template>
 
 <script>
+import ListAdd from './ListAdd.vue'
+
 export default {
-  
+  components: {
+    ListAdd
+  },
 }
 </script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- 
+    v-on ディレクティブでイベントをハンドルでき、 @ は v-on の省略記法になる                                                 
+    .prevent はVueイベントの修飾詞であり、サブミット時にリロードされるのを防ぐ 
+  -->
+  <form class="addlist" @submit.prevent="addList">
+    <!-- 
+      v-model に titleを定義することで、 data プロパティとバインドさせます 
+      データをバインドさせる（データバインディング）とは、scriptで定義した data プロパティをテンプレート
+      でもリアルタイムにそのまま共有（紐付け）できるような仕組みです
+      v-model は v-on と v-bind の２つの機能を持ち合わせている
+
+      下記は同じ実装になる
+        <input v-model="title">
+        <input :value="title" @input"title = $event.target.value">
+      :value="title" でスクリプト内の data プロパティで定義した title を <input> の value 属性と紐付けています
+      @input"title = $event.target.value" で <input> フォームに値が入力された際にその値を title に代入します
+
+      v-model は input、select、textareaなどのフォーム要素に対して使用することができる
+    -->
+    <input v-model="title"
+           type="text"
+           class="text-input"
+           placeholder="Add new list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: '',
+    }
+  },
+  methods: {
+    addList: function() {
+      // this.$store.dispatch('アクション名') で actions を実行できます
+      this.$store.dispatch('addList', { title: this.title })
+      this.title = ''
+    },
+  }
+}
+</script>
