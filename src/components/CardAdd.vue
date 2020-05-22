f1ff087d137990c483b6962aa47255ab09d11f8b
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-23 00:06:13 +0900

    :sparkles: 4-1 CardAddコンポーネントを作成しよう 4.フォームにテキストが入力されたらボタンを活性にしよう

--- src/components/CardAdd.vue
+++ src/components/CardAdd.vue
@@ -9,8 +9,10 @@
            @focusin="startEditing"
            @focusout="finishEditing"
     />
+    <!-- フォームが活性の時(isEditing)、テキストが入力された時(bodyExists)のどちらかが true の時だけ表示される -->
     <button type="submit"
-            class="add-button">
+            class="add-button"
+            v-if="isEditing || bodyExists">
       Add
     </button>
   </form>
@@ -34,7 +36,11 @@ export default {
     classList() {
       const classList = ['addcard']
       if (this.isEditing) classList.push('active')
+      if (this.bodyExists) classList.push('addable')
       return classList
+    },
+    bodyExists() {
+      return this.body.length > 0 
     }
   },
   methods: {
