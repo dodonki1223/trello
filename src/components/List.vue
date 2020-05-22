0ad0a7eb1ff8025bef85b0278d75298505a79aab
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-22 09:01:03 +0900

    :sparkles: 3-3 Listコンポーネントを作成しよう 1.リストを作成しよう(List.vue)

---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <!-- props で定義している title の値が変更されるたびに変わります -->
+      <p class="list-title">{{ title }}</p>
+      <!-- removeListメソッドをハンドルしている -->
+      <div class="deletelist" @click="removeList">x</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // propsについては下記ドキュメントを参照すること
+  //   https://jp.vuejs.org/v2/guide/components-props.html
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    removeList: function() {
+      if(confirm('本当にこのリストを削除しますか？')){
+        this.$store.dispatch('removeList', { listIndex: this.listIndex })
+      }
+    }
+  }
+}
+</script>
