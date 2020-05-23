4c65343c6c405ac0a624d3da07a173e0f2537ff6
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-23 20:53:31 +0900

    :sparkles: 4-3 カードの総数を取得しよう 1.リスト内のカードの総数を表示しよう(List.vue)

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,6 +3,7 @@
     <div class="list-header">
       <!-- props で定義している title の値が変更されるたびに変わります -->
       <p class="list-title">{{ title }}</p>
+      <p class="list-counter">total: {{ totalCardInList }}</p>
       <!-- removeListメソッドをハンドルしている -->
       <div class="delete-list" @click="removeList">x</div>
     </div>
@@ -44,6 +45,16 @@ export default {
     listIndex: {
       type: Number,
       required: true
+    }
+  },
+  computed: {
+    // リスト内のカードの総数は、カードが作成されたり、削除されるごとにリアルタイムで更新される必要がある
+    // 算出プロパティ (computed) は、テンプレートにデータをそのまま、表示させるのではなく、
+    // データを加算や加工させたものを表示させる際に便利
+    // methods 等に this.${computedで定義したプロパティ名}のように呼び出すことができます
+    // 指定したデータに変更があるたびに computed は算出したデータを返してくれます
+    totalCardInList() {
+      return this.cards.length
     }
   },
   methods: {
